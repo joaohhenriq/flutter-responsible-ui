0a4a962f567c18f4d2100bf93a59b6c638cc762e
joaoh <joao_camargo10@hotmail.com>
2019-08-14 21:40:42 -0300

    Bottombar and animations

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_responsible_ui/commom/app_bottom_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> _heightFactorAnimation;
+  final double collapsedHeightFactor = 0.85;
+  final double expandedHeightFactor = 0.67;
+  bool isAnimationCompleted = false;
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    _heightFactorAnimation =
+        Tween<double>(begin: collapsedHeightFactor, end: expandedHeightFactor)
+            .animate(_controller);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  onBottomPartTap() {
+    setState(() {
+      if (isAnimationCompleted) {
+        _controller.reverse();
+      } else {
+        _controller.forward();
+      }
+
+      isAnimationCompleted = !isAnimationCompleted;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Image.asset(
+      bottomNavigationBar: AppBottomBar(),
+      body: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return getWidget();
+        },
+      ),
+    );
+  }
+
+  Widget getWidget() {
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        FractionallySizedBox(
+          alignment: Alignment.topCenter,
+          heightFactor: _heightFactorAnimation.value,
+          child: Image.asset(
             "assets/images/tokyo.jpg",
             fit: BoxFit.cover,
-            colorBlendMode: BlendMode.hue,
-            color: Colors.black,
-          )
-        ],
-      ),
+//            colorBlendMode: BlendMode.hue,
+//            color: Colors.black,
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            onBottomPartTap();
+          },
+          child: FractionallySizedBox(
+            alignment: Alignment.bottomCenter,
+            heightFactor: 1.05 - _heightFactorAnimation.value,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.blue[200],
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
