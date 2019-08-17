faa769e752627bd45d2f3651ba4ac3713d4c92b3
joaoh <joao_camargo10@hotmail.com>
2019-08-17 09:57:32 -0300

    Finishing the app

--- lib/commom/app_bottom_bar.dart
+++ lib/commom/app_bottom_bar.dart
@@ -11,16 +11,49 @@ class AppBottomBar extends StatelessWidget {
           topRight: Radius.circular(30),
         ),
       ),
-      padding: EdgeInsets.only(bottom: 18, left: 24, right: 24, top: 16),
+      padding: EdgeInsets.only(bottom: 8, left: 24, right: 24, top: 5),
       child: Row(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          Icon(Icons.library_books, color: Colors.black,),
-          Icon(Icons.search, color: Colors.black,),
-          Icon(Icons.add, color: Colors.black,),
-          Icon(Icons.alarm, color: Colors.black,),
-          Icon(Icons.chat, color: Colors.black,),
+          Icon(
+            Icons.library_books,
+            color: Colors.grey,
+          ),
+          Icon(
+            Icons.search,
+            color: Colors.grey[850],
+          ),
+          Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+              color: Colors.grey[850],
+              borderRadius: BorderRadius.all(
+                Radius.circular(40),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0, 2),
+                  blurRadius: 2,
+                  spreadRadius: 0.1
+                ),
+              ],
+            ),
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          ),
+          Icon(
+            Icons.alarm,
+            color: Colors.grey,
+          ),
+          Icon(
+            Icons.chat,
+            color: Colors.grey,
+          ),
         ],
       ),
     );
